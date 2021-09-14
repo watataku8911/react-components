2ce20894e2e6a1d2875adafa5175154c62491e01
watataku8911 <watato.takuya@gmail.com>
2021-09-14 17:05:53 +0900

    add/button add/animationx

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../css/UIKit.css";
+
+type Props = {
+  label: string;
+  disabled: boolean;
+  onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+};
+
+const Button = (props: Props) => {
+  return (
+    <button
+      className="button"
+      disabled={props.disabled}
+      onClick={props.onClick}
+    >
+      {props.label}
+    </button>
+  );
+};
+export default Button;
